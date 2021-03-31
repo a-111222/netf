f4e075749722802476b6d61a4a9d9b059a30967f
Abdelrhman Ahmed <57258659+a-111222@users.noreply.github.com>
2021-03-31 18:53:42 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Comp from './Comp';
+function App() {
+
+  return (
+    
+    <div className="App">
+     <h1> GET DATA FROM JSON FILE BY Axios Lib. </h1>
+      <Comp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Comp.js
@@ -0,0 +1,38 @@
+import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react';
+
+export default class comp extends Component {
+    state={
+        persons:[],
+    }
+    componentDidMount(){
+        axios.get('./data.json').then(res=>{
+            console.log(res.data);
+            this.setState({persons:res.data});
+        })
+    }
+    render() {
+        return (
+            <div>
+                <table className="table">
+                <th>Name</th>
+                <th>Age</th>
+                <th>Address</th>
+             
+
+                {this.state.persons.map(person=>
+                    
+                    <tr key="id">
+                        <td>{person.name}</td>
+                        <td>{person.age}</td>
+                        <td>{person.city}</td>
+
+
+
+                    </tr> )}
+                       </table>
+            </div>
+        )
+    }
+}
