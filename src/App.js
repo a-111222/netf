dcb715697f56a6f758aab54c414660aac43a22d1
Abdelrhman Ahmed <57258659+a-111222@users.noreply.github.com>
2021-04-03 18:18:39 +0200

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     
     <div className="App">
      <h1 className="main-title"> NETFLIX </h1>
-     <p>الأفلام الاكثر مشاهدة </p>
+     <p>.الأفلام الاكثر مشاهدة </p>
       <Comp/>
     </div>
   );
